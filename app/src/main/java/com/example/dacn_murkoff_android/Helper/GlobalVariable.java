43663a5a0145c78070b1877e37bf556332ab7c93
duy_pham <phambachanduy@gmail.com>
2024-10-19 22:46:48 +0700

    Update GlobalVariable

---
+++ app/src/main/java/com/example/dacn_murkoff_android/Helper/GlobalVariable.java
@@ -0,0 +1,95 @@
+package com.example.dacn_murkoff_android.Helper;
+
+import android.app.Application;
+
+import com.example.dacn_murkoff_android.Model.Option;
+import com.example.dacn_murkoff_android.Model.User;
+import com.example.dacn_murkoff_android.R;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class GlobalVariable extends Application {
+    private String accessToken;
+    private User AuthUser;
+    private final String SHARED_PREFERENCE_KEY = "dacnmurkoff";
+    private String contentType = "application/x-www-form-urlencoded";
+
+    private Map<String, String> headers;
+
+
+    /*Thiết lập và trả về một HTTP request header khi ứng dụng gửi yêu cầu HTTP đến máy chủ*/
+    public Map<String, String> getHeaders() {
+
+        this.headers = new HashMap<>();
+        this.headers.put("Content-Type", contentType );
+        this.headers.put("Authorization", accessToken);
+        this.headers.put("type", "patient");
+
+        return headers;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public void setAccessToken(String accessToken) {
+        this.accessToken = accessToken;
+    }
+
+    public User getAuthUser() {
+        return AuthUser;
+    }
+
+    public void setAuthUser(User authUser) {
+        AuthUser = authUser;
+    }
+
+    public String getSharedReferenceKey() {
+        return SHARED_PREFERENCE_KEY;
+    }
+
+    public String getContentType() {
+        return contentType;
+    }
+
+    public void setContentType(String contentType) {
+        this.contentType = contentType;
+    }
+
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    /*Tùy Chọn Lọc*/
+    public List<Option> getFilterOptions()
+    {
+        List<Option> list = new ArrayList<>();
+
+        //Option option0 = new Option();
+        //option0.setIcon(R.drawable.ic_all);
+        //option0.setName(getString(R.string.all));
+
+        Option option1 = new Option();
+        option1.setIcon(R.drawable.ic_service);
+        option1.setName(getString(R.string.service));
+
+        Option option2 = new Option();
+        option2.setIcon(R.drawable.ic_speciality);
+        option2.setName(getString(R.string.speciality));
+
+        Option option3 = new Option();
+        option3.setIcon(R.drawable.ic_doctor);
+        option3.setName(getString(R.string.doctor));
+
+        list.add(option1);
+        list.add(option2);
+        list.add(option3);
+
+        return list;
+    }
+
+}
---
+++ app/src/main/java/com/example/dacn_murkoff_android/HomePage/HomePageFragment.java
@@ -0,0 +1,99 @@
+package com.example.dacn_murkoff_android.HomePage;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.example.dacn_murkoff_android.Helper.GlobalVariable;
+import com.example.dacn_murkoff_android.R;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class HomePageFragment extends Fragment {
+
+    private final String TAG = "Home Page Fragment";
+
+    private GlobalVariable globalVariable;
+
+    private RecyclerView recyclerViewSpeciality;
+    private RecyclerView recyclerViewDoctor;
+    private RecyclerView recyclerViewHandbook;
+    private RecyclerView recyclerViewRecommendedPages;
+
+
+    private EditText searchBar;
+    private TextView txtReadMoreSpeciality;
+    private TextView txtReadMoreDoctor;
+
+    private Context context;
+    private RecyclerView recyclerViewButton;
+
+    private TextView txtDate;
+    private TextView txtWeather;
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_home_page, container, false);
+        setupComponent(view);
+
+
+        return view;
+    }
+
+    private void setupComponent(View view)
+    {
+        context = requireContext();
+        globalVariable = (GlobalVariable) requireActivity().getApplication();
+
+//        recyclerViewSpeciality = view.findViewById(R.id.recyclerViewSpeciality);
+//        recyclerViewDoctor = view.findViewById(R.id.recyclerViewDoctor);
+//        recyclerViewButton = view.findViewById(R.id.recyclerViewButton);
+//        recyclerViewHandbook = view.findViewById(R.id.recyclerViewHandbook);
+//        recyclerViewRecommendedPages = view.findViewById(R.id.recyclerViewRecommendedPages);
+//
+//        searchBar = view.findViewById(R.id.searchBar);
+//        txtReadMoreSpeciality = view.findViewById(R.id.txtReadMoreSpeciality);
+//        txtReadMoreDoctor = view.findViewById(R.id.txtReadMoreDoctor);
+//
+//        txtWeather = view.findViewById(R.id.txtWeather);
+//        txtDate = view.findViewById(R.id.txtDate);
+
+    }
+
+
+    private void setupViewModel()
+    {
+        /*Step 1 - Khởi tạo ViewModel*/
+
+
+        /*Step 2 - Chuẩn bị header & tham số*/
+
+
+
+        /*Step 3 - listen speciality Read All */
+
+
+
+        /*Step 4 - listen doctor read all*/
+
+    }
+
+
+
+}
